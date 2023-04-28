c1fe453527061f5f6ab458a50d9284d4f90b059c
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-04-28 19:14:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-// const primeiroValor = 345
-// const segundoValor = 10
+const primeiroValor = 345
+const segundoValor = 10
 
-// const resultado = primeiroValor / segundoValor
+const resultado = primeiroValor / segundoValor
 
-// console.log(resultado)
+console.log(resultado)
 
 
 
-// let soma = 3
-// soma += 4
-// console.log("a soma de 3 com 4 é igual a:",(soma), )
-// =7
+let soma = 3
+soma += 4
+console.log("a soma de 3 com 4 é igual a:",(soma), )
+=7
 
-// let mult = 3 
-// mult *= 5
-// console.log("a multiplicação de 3 com 5 é igual a" ,(mult), "e se dividida por 2, é igual a" ,(mult / 2), )
-// =7,5
+let mult = 3 
+mult *= 5
+console.log("a multiplicação de 3 com 5 é igual a" ,(mult), "e se dividida por 2, é igual a" ,(mult / 2), )
+=7,5
 
-// let subt = 4
-// subt -= 5
-// console.log("a subtração de 5 por 4 é igual a" ,(subt), "e se multiplicado por -1, é igual a" ,(subt * -1), );
-// =1
+let subt = 4
+subt -= 5
+console.log("a subtração de 5 por 4 é igual a" ,(subt), "e se multiplicado por -1, é igual a" ,(subt * -1), );
+=1
 
-// let rest = 234
-// rest %= 5
-// console.log("o resto da divisão entre 234 e 5 é igual a" ,(rest), );
-// =4
+let rest = 234
+rest %= 5
+console.log("o resto da divisão entre 234 e 5 é igual a" ,(rest), );
+=4
 
-// comparadores 
+comparadores 
 
-// console.log(1 === 2);
-// const primeiroNumero = 10
-// const segundoNumero = 20
-// console.log(primeiroNumero === segundoNumero); //false
-// console.log(primeiroNumero !== segundoNumero); //true
-// console.log(primeiroNumero > segundoNumero); //false
-// console.log(primeiroNumero >= segundoNumero); //false
-// console.log(primeiroNumero < segundoNumero); //true
-// console.log(primeiroNumero <= segundoNumero); //true
+console.log(1 === 2);
+const primeiroNumero = 10
+const segundoNumero = 20
+console.log(primeiroNumero === segundoNumero); //false
+console.log(primeiroNumero !== segundoNumero); //true
+console.log(primeiroNumero > segundoNumero); //false
+console.log(primeiroNumero >= segundoNumero); //false
+console.log(primeiroNumero < segundoNumero); //true
+console.log(primeiroNumero <= segundoNumero); //true
 
-// const nome = prompt("Digite seu nome")
-// const idade = Number (prompt("Digite sua idade"))
-// console.log("Nome:" , (nome), )
-// console.log("Idade:" , (idade), )
-// console.log("É maior de idade" , (idade > 18), );
-// console.log("Em 2050 você terá" , (idade + 27), )
+const nome = prompt("Digite seu nome")
+const idade = Number (prompt("Digite sua idade"))
+console.log("Nome:" , (nome), )
+console.log("Idade:" , (idade), )
+console.log("É maior de idade" , (idade > 18), );
+console.log("Em 2050 você terá" , (idade + 27), )
 
-//console.log(new Date().getFullYear()) <<<<<<<<<< data completa
+console.log(new Date().getFullYear()) <<<<<<<<<< data completa
 
 
 
